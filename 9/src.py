b6a5c5b3386a063376c88bdca18e3abedf62b406
D.P. Bastings <dp.bastings@posteo.net>
2022-12-09 23:55:16 +0100

    Completed day 9. I've really outdone myself here (and I mean that in the most disparaging way possible...).

--- 9/src.py
+++ 9/src.py
@@ -1,114 +1,127 @@
-"""Written in Python 2.7.16."""
+"""Written in Python 3.9.2."""
 
-XDIM = 512
-YDIM = 512
+XDIM = 256
+YDIM = 256
 START = (XDIM // 2, (YDIM - 1) // 2)
-INSTRUCTIONS = {"R": (1, 0), "L": (-1, 0), "U": (0, 1), "D": (0, -1)}
 
 class Instruction(object):
     def __init__(self, direction, dist):
         self.direction = direction
         self.dist = int(dist)
     def __repr__(self):
-        return "{0} {1}".format(self.direction, self.dist)
+        return f"{self.direction}, {self.dist}"
 
 
 class Knot(object):
-    def __init__(self, x, y, head=None):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.head = head
-
-    def move(self, direction, dist):
-        if direction == "R":
-            self.x += 1
-        elif direction == "L":
-            self.x -= 1
-        elif direction == "U":
-            self.y -= 1
-        elif direction == "D":
-            self.y += 1
-
-    def follow(self):
-        if self.head.x > self.x + 1:
-            self.y = self.head.y
-            self.x = self.head.x - 1
-        elif self.head.x < self.x - 1:
-            self.y = self.head.y
-            self.x = self.head.x + 1
-        elif self.head.y > self.y + 1:
-            self.x = self.head.x
-            self.y = self.head.y - 1
-        elif self.head.y < self.y - 1:
-            self.x = self.head.x
-            self.y = self.head.y + 1
+    
+    def follow(self, prev):
+        if prev.x > self.x + 1:
+            self.x = prev.x - 1
+            if prev.y > self.y:
+                self.y += 1
+            elif prev.y < self.y:
+                self.y -= 1
+        elif prev.x < self.x - 1:
+            self.x = prev.x + 1
+            if prev.y > self.y:
+                self.y += 1
+            elif prev.y < self.y:
+                self.y -= 1
+        elif prev.y > self.y + 1:
+            self.y = prev.y - 1
+            if prev.x > self.x:
+                self.x += 1
+            elif prev.x < self.x:
+                self.x -= 1
+        elif prev.y < self.y - 1:
+            self.y = prev.y + 1
+            if prev.x > self.x:
+                self.x += 1
+            elif prev.x < self.x:
+                self.x -= 1
 
 class Rope(object):
-    def __init__(self, field, head, knots):
+    def __init__(self, knots, field, start=START):
         self.field = field
-        self.knots = [head]
-        for n in range(0, knots):
-            k = Knot(self.head().x, self.head().y, self.knots[n])
-            self.knots.append(k)
-    
+        self.knots = [Knot(*start) for _ in range(0, knots)]
+
     def head(self):
         return self.knots[0]
-
+    
     def tail(self):
         return self.knots[-1]
     
-    def move(self, direction, dist):
-        self.head().move(direction, dist)
-        for k in self.knots[1:]:
-            k.follow()
-        self.field[self.tail().y][self.tail().x] = '#'
+    def move(self, direction):
+        if "R" in direction:
+            self.head().x += 1
+        if "L" in direction:
+            self.head().x -= 1
+        if "U" in direction:
+            self.head().y -= 1
+        if "D" in direction:
+            self.head().y += 1
+        for n, k in enumerate(self.knots[1:]):
+            k.follow(self.knots[n])
+    
+    def display(self):
+        self.field[self.head().y][self.head().x].value = 'H'
+        for n, k in enumerate(self.knots[1:]):
+            self.field[k.y][k.x].value = n + 1
+
+
+class Cell():
+    def __init__(self, value):
+        self.value = value
+    
+    def __repr__(self):
+        return f"{self.value}"
 
 class Field(list):
     def __init__(self):
         super(list, self).__init__()
         for _ in range (0, YDIM):
-            self.append(["." for _ in range(0, XDIM)])
-
+            self.append([Cell(".") for _ in range(0, XDIM)])
+        self[START[1]][START[0]].value = 's'
+    
+    def reset(self):
+        for r in self:
+            for c in r:
+                c.value = "."
+        self[START[1]][START[0]].value = 's'
+    
     def view(self):
         for r in self:
             for c in r:
-                print c,
-            print ""
+                print(c, end="")
+            print("")
     
-    def export(self):
-        s = ""
-        for r in self:
-            for c in r:
-                s += c,
-            s += '\n'
-        return s
-
     def check_visited(self):
         x = 0
         for r in self:
             for c in r:
-                if c == '#' or c == 's':
+                if c.value == '#' or c.value == 's':
                     x += 1
         return x
 
 def parse(line):
     return (Instruction(*line.split()))
 
-def run():
+def run(knots):
     with open(input("input: "), 'r') as f:
         instructions = [parse(l) for l in f.readlines()] 
     field = Field() 
-    rope = Rope(field, Knot(*START), 9)
+    rope = Rope(knots, field)
     for n, i in enumerate(instructions):
         for _ in range(0, i.dist):
-            try:
-                rope.move(i.direction, 1)
-            except IndexError:
-                print n, i
-                quit()
-    field[START[1]][START[0]] = 's'
+            rope.move(i.direction)
+            field[rope.tail().y][rope.tail().x].value = '#'
+        #rope.display()
+        #field.view()
+        #print("")
+        #field.reset()
     #field.view()
-    print "The tail has visited {0} positions.".format(field.check_visited()) 
+    print(f"The tail has visited {field.check_visited()} positions.")
     #return field.export()
-
-run()
